bc0017157dc0e77fbdb4554eaf8a0744bec425c7
Yuzaw <fatihbari37@gmail.com>
2024-11-19 17:53:54 +0700

    adding database

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const API = require('./routes/API');
+const sequelize = require('./config/database');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/API', API);
+
+// Sinkronisasi database
+sequelize.sync({ force: false }) // force: true untuk mengganti tabel jika ada perubahan
+  .then(() => {
+    console.log('Database synced');
+  })
+  .catch((err) => console.error('Error syncing database:', err));
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
